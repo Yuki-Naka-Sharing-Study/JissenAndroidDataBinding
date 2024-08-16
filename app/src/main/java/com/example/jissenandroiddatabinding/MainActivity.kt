d98ec14cedc08748b5d13c3e8b1468d2969fc31e
Yuki Naka <yukinaka2168@au.com>
2024-08-16 14:57:04 +0900

    1個目のアプリ作成完了。ビルドも通った。

--- app/src/main/java/com/example/jissenandroiddatabinding/MainActivity.kt
+++ app/src/main/java/com/example/jissenandroiddatabinding/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.jissenandroiddatabinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.example.jissenandroiddatabinding.MainActivity
 import com.example.jissenandroiddatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        binding.counter = 1
+
+        binding.sampleButton.setOnClickListener {
+            binding.counter += 1
+        }
     }
 }
