939e29cae84868b3edd0a56ff91ce78c98d98a44
Yuki Naka <yukinaka2168@au.com>
2024-08-17 10:34:20 +0900

    5個目のアプリ作成完了。ビルドも通った。

---
+++ app/src/main/java/com/example/jissenandroiddatabinding/ListItem.kt
@@ -0,0 +1,5 @@
+package com.example.jissenandroiddatabinding
+
+import java.util.Date
+
+data class ListItem(val text: String, val due: Date)
--- app/src/main/java/com/example/jissenandroiddatabinding/MainActivity.kt
+++ app/src/main/java/com/example/jissenandroiddatabinding/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.jissenandroiddatabinding
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.jissenandroiddatabinding.databinding.ActivityMainBinding
+import com.example.jissenandroiddatabinding.databinding.ListExampleBinding
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +18,25 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
             this, R.layout.activity_main
         )
-        binding.setOnClick {
-            binding.text = "Click!"
+        val adapter = MainListAdapter(this).apply {
+            add(ListItem("ゴミ出し", Date()))
+            add(ListItem("技術書を書く", Date()))
+            add(ListItem("脱稿する", Date()))
+        }
+        binding.listView.adapter = adapter
+    }
+
+    class MainListAdapter(context: Context): ArrayAdapter<ListItem>(context, 0) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val binding: ListExampleBinding
+            if (convertView == null) {
+                binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.list_example, parent, false)
+                binding.root.tag = binding
+            } else {
+                binding = convertView.tag as ListExampleBinding
+            }
+            binding.item = getItem(position)
+            return binding.root
         }
     }
 }
