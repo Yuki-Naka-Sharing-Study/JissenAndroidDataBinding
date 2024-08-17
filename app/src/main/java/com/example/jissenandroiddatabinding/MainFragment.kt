5f74b8b4b67a27bf992f7187aa37c280bc6d6ad2
Yuki Naka <yukinaka2168@au.com>
2024-08-17 11:11:37 +0900

    6個目のアプリ作成完了。ビルドも通った。

---
+++ app/src/main/java/com/example/jissenandroiddatabinding/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.jissenandroiddatabinding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.jissenandroiddatabinding.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonKinoko.setOnClickListener {
+            binding.kinoko += 1
+        }
+
+        binding.buttonTakenoko.setOnClickListener {
+            binding.takenoko += 1
+        }
+    }
+}
