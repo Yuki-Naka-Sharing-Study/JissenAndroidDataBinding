5f932c236067bd3bf335f5b3a672078bcac77e5f
Yuki Naka <yukinaka2168@au.com>
2024-08-18 17:05:18 +0900

    最後のアプリ完成。

--- app/src/main/java/com/example/jissenandroiddatabinding/ListItem.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jissenandroiddatabinding
-
-import java.util.Date
-
-data class ListItem(val text: String, val due: Date)
--- app/src/main/java/com/example/jissenandroiddatabinding/MainActivity.kt
+++ app/src/main/java/com/example/jissenandroiddatabinding/MainActivity.kt
@@ -1,61 +1,16 @@
 package com.example.jissenandroiddatabinding
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.jissenandroiddatabinding.databinding.ActivityMainBinding
-import com.example.jissenandroiddatabinding.databinding.ListRepositoryBinding
 
-// 続き 第4章 Build GitHub Search Repos App
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private val viewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewModel = viewModel
-
-        val adapter = RepositoryAdapter(this)
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.adapter = adapter
-
-        viewModel.repositories.observe(this, Observer { response ->
-            response?.let { adapter.items = it }
-        })
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.value = null
     }
 
-}
-
-class RepositoryAdapter(context: Context) : RecyclerView.Adapter<RepositoryAdapter.Holder>() {
-    private val inflater = LayoutInflater.from(context)
-    var items: List<RepositoryItem> = emptyList()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getItemCount(): Int = items.size
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding: ListRepositoryBinding = DataBindingUtil.inflate(
-            inflater, R.layout.list_repository, parent, false
-        )
-        return Holder(binding)
-    }
-
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.binding.item = items[position]
-        holder.binding.executePendingBindings()
-    }
-
-    class Holder(val binding: ListRepositoryBinding): RecyclerView.ViewHolder(binding.root)
 }
